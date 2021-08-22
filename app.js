0461b9118c108aafc37235e834f1198b6bc815aa
Abdul-Faiz <53165415+AbdulFaiz123@users.noreply.github.com>
2021-08-22 14:57:32 +0530

    Sum commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+
+const luckyNumber = document.querySelector("#lucky-number");
+
+const checkNumber = document.querySelector("#check-number");
+
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+}
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+   
+    for(let i = 0;i<dob.length;i++){
+     
+        sum = sum + Number(dob.charAt(i)); 
+    }
+    return sum;
+}
+
+checkNumber.addEventListener("click",checkBirthDateIsLucky);
